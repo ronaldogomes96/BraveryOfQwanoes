8b31cc0df2abe0285f65e6fe0fb07b6bfbf3f2dc
Anderson Alencar <andersonalencar@MacBook-Pro-de-Anderson-2.local>
2021-03-18 11:08:24 -0300

    add background earth

--- BraveryOfQwanoes/BraveryOfQwanoes/Scenes/EarthScene.swift
+++ BraveryOfQwanoes/BraveryOfQwanoes/Scenes/EarthScene.swift
@@ -11,9 +11,17 @@ import SpriteKit
 
 class EarthScene: SKScene {
     
+    var background = Background(name: "background_grande")
+    
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         self.backgroundColor = .purple
+        setupNodePosition()
     }
-
+    
+    func setupNodePosition() {
+        guard let backgroundComponent = background.component(ofType: DynamicBackgroundComponent.self)?.backgroundNode else {return}
+        backgroundComponent.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        self.addChild(backgroundComponent)
+    }
 }
