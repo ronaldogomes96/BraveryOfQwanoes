5c7e241006eb927c000756a96830372aeec3b57f
Anderson Alencar <andersonalencar@MacBook-Pro-de-Anderson.local>
2021-04-05 09:19:00 -0300

    first version background enceladus

--- BraveryOfQwanoes/BraveryOfQwanoes/Scenes/EnceladusScene.swift
+++ BraveryOfQwanoes/BraveryOfQwanoes/Scenes/EnceladusScene.swift
@@ -10,4 +10,23 @@ import SpriteKit
 
 class EnceladusScene: SKScene {
     
+    let background = Background(name: "nome de bacnground")
+    let character = CharacterBoat(characterName: "qwanoes_happy")
+    var jsonNames = ["Introduction", "PartOne", "PartTwo", "PartThree", "PartFour", "PartFive"]
+    var dialog = Dialog(historyPart: "PartOne")
+    var dialogNodes = [SKLabelNode]()
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        setupBackgroundNode()
+    }
+    
+    
+    func setupBackgroundNode() {
+        guard let backgroundComponent = background.component(ofType: DynamicBackgroundComponent.self)?.backgroundNode else {return}
+        backgroundComponent.size.width = self.size.width * 2 // verificar esse
+        backgroundComponent.size.height = self.size.height
+        backgroundComponent.position = CGPoint(x: backgroundComponent.size.width/2, y: self.frame.midY) //verificar esse
+        self.addChild(backgroundComponent)
+    }
 }
